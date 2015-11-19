27bd8e7ad2325c00f3e05e83ed8a7b088d606ee1
Brett Wise <brettdwise@gmail.com>
2015-11-19 11:04:10 -0500

    Spawn1 completed

--- lib/spawn.ex
+++ lib/spawn.ex
@@ -1,5 +1,15 @@
-defmodule SpawnBasic do
+defmodule Spawn1 do
   def greet do
-    IO.puts "Hello"
+    receive do
+      {sender, msg} ->
+        send sender, {:ok, "Hello, from one asshole to another #{msg}"}
+    end
   end
+end
+# here's a client
+pid = spawn(Spawn1, :greet, [])
+send pid, {self, "asshole!"}
+receive do
+  {:ok, message} ->
+    IO.puts message
 end
